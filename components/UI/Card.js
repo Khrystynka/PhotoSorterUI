dadf02e263175bafe3425a5a1bf8dd42d4191f4c
Khrystyna Boychuk <kh.hamal@gmail.com>
2021-03-09 17:38:02 -0800

    Added option to select from previously used tags

--- components/UI/Card.js
+++ components/UI/Card.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import {View,StyleSheet} from 'react-native'
+import React from "react";
+import { View, StyleSheet } from "react-native";
 
-const Card = props =>{
-    return <View style={{...styles.card, ...props.style}}>
-        {props.children}
-    </View>
-}
+const Card = (props) => {
+	return (
+		<View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+	);
+};
 
 const styles = StyleSheet.create({
-    card:{
-        shadowColor:'black',
-shadowOpacity:0.3,
-shadowOffset:{width:0,height:2},
-shadowRadius:8,
-elevation:5,
-borderRadius:10,
-backgroundColor:'white',
-width:'90%',
-
-    }
+	card: {
+		shadowColor: "black",
+		shadowOpacity: 0.3,
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 8,
+		elevation: 5,
+		borderRadius: 10,
+		backgroundColor: "white",
+		width: "90%",
+		height: "100%",
+		// justifyContent: "space-between",
+	},
 });
 export default Card;
--- screens/uploads/AddDocumentScreen.js
+++ screens/uploads/AddDocumentScreen.js
@@ -1,243 +1,267 @@
 // import { StatusBar } from 'expo-status-bar';
-import React , {useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux'
-import { TextInput,StyleSheet,View,Text,TouchableOpacity,Image,Button} from 'react-native';
-import * as DocumentPicker from 'expo-document-picker';
-import Colors from '../../constants/Colors'
-import Card from '../../components/UI/Card'
-import * as documentActions from '../../store/actions/documents'
-
-
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	TextInput,
+	StyleSheet,
+	View,
+	Text,
+	TouchableOpacity,
+	Image,
+	Button,
+} from "react-native";
+import * as DocumentPicker from "expo-document-picker";
+import Colors from "../../constants/Colors";
+import Card from "../../components/UI/Card";
+import * as documentActions from "../../store/actions/documents";
 
 const AddDocumentScreen = (props) => {
-const [newDocument,setNewDocument]= useState(null)
-  const [tagList, setTagList] = useState([]);
-  const [newTag,setNewTag] = useState('')
-  const userId = useSelector(state => state.auth.id)
-  const token = useSelector(state =>state.auth.token)
-  console.log('userId',userId)
-  console.log('token',token)
+	const [newDocument, setNewDocument] = useState(null);
+	const [tagList, setTagList] = useState([]);
+	const [newTag, setNewTag] = useState("");
+	const userId = useSelector((state) => state.auth.id);
+	const token = useSelector((state) => state.auth.token);
+	const userTags = useSelector((state) => state.documents.userTags);
+	console.log("userId", userId);
+	console.log("token", token);
 
-const dispatch=useDispatch()
-  const createDocumentHandler = (doc,tags,userId,token) => {
+	const dispatch = useDispatch();
+	const createDocumentHandler = (doc, tags, userId, token) => {
+		console.log(
+			"creating document with tags",
+			tags,
+			"title",
+			doc.name,
+			"owner",
+			userId
+		);
+		dispatch(documentActions.createDocument(doc, tags, userId, token));
+		props.navigation.goBack();
+	};
+	const addTagHandler = () => {
+		console.log("Updating document");
+		setTagList([...tagList, newTag]);
+		setNewTag("");
+	};
+	const inputChangedHandler = (input) => {
+		setNewTag(input);
+		// e.target.value=''
+	};
+	const onDeleteTagHandler = (ind) => {
+		console.log("Deleting tag", tagList[ind]);
+		const newTagList = tagList.filter((item, index) => index !== ind);
+		// setNewTag(input)
+		setTagList(newTagList);
+		// e.target.value=''
+	};
+	const onAddPrevTagHandler = (ind) => {
+		console.log("adding tag", tagList[ind]);
+		const newTagList = tagList.concat(userTags[ind]);
+		setTagList(newTagList);
+	};
+	const tagArray = tagList.map((item, index) => (
+		<TouchableOpacity onPress={() => onDeleteTagHandler(index)} key={index}>
+			<View style={styles.tag}>
+				<Text style={styles.tagText}>{item}</Text>
+			</View>
+		</TouchableOpacity>
+	));
 
-      console.log('creating document with tags',tags,'title',doc.name,'owner',userId)
-      dispatch(documentActions.createDocument(doc,tags,userId,token))
-      props.navigation.goBack()
-    }
-  const addTagHandler=()=>{
-    console.log('Updating document')
-    setTagList([...tagList,newTag]);
-    setNewTag('')
+	const userTagsArray = userTags.map((item, index) => (
+		<TouchableOpacity onPress={() => onAddPrevTagHandler(index)} key={index}>
+			<View style={styles.tag}>
+				<Text style={styles.tagText}>{item}</Text>
+			</View>
+		</TouchableOpacity>
+	));
+	const selectFile = async () => {
+		// Opening Document Picker to select one file
+		try {
+			const res = await DocumentPicker.getDocumentAsync();
+			console.log("type", res, res.type);
 
-  }
-  const inputChangedHandler = input => {
-      setNewTag(input)
-      // e.target.value=''
-    }
-  const onDeleteTagHandler = ind => {
-    console.log('Deleting tag',tagList[ind])
-      const newTagList= tagList.filter((item,index)=>index !==ind)
-      // setNewTag(input)
-      setTagList(newTagList)
-      // e.target.value=''
-    }
-  const tagArray=tagList.map((item,index)=><TouchableOpacity onPress={()=>onDeleteTagHandler(index)} key={index}>
-    <View style = {styles.tag}  >
-      <Text style={styles.tagText}>{item}</Text>
-      </View>
-      </TouchableOpacity>)
+			if (res.type == "success") {
+				console.log("EVERYTHING I FINE", res.uri);
+				setNewDocument(res);
+			} else {
+				console.log("not here");
 
+				// setNewDocument({uri:'',name:''})
+				setNewDocument(null);
+			}
+		} catch (err) {
+			console.log("not here");
 
-  
-  const selectFile = async () => {
-    // Opening Document Picker to select one file
-    try {
-      const res = await DocumentPicker.getDocumentAsync()
-      console.log('type',res,res.type)
+			// setNewDocument({uri:'',name:''})
+			setNewDocument(null);
 
-      if (res.type =='success'){
-        console.log('EVERYTHING I FINE', res.uri)
-        setNewDocument(res)
+			throw err;
+		}
+	};
 
+	let documentDetails = <Text>No file picked</Text>;
+	let previousTags = null;
 
-      }
-      else{
-        console.log("not here")
-        
-        // setNewDocument({uri:'',name:''})
-        setNewDocument(null)
+	if (newDocument) {
+		documentDetails = (
+			<View>
+				<View style={styles.imgContainer}>
+					<Image style={styles.image} source={{ uri: newDocument.uri }} />
+				</View>
+				<View style={styles.tagContainer}>
+					<Text style={styles.title}>Add/remove tags</Text>
+					<View style={styles.tagList}>{tagArray}</View>
+					<View style={styles.form}>
+						<View style={styles.inputGroup}>
+							<TextInput
+								style={styles.input}
+								keyboardType="default"
+								onChangeText={inputChangedHandler}
+								value={newTag}
+							></TextInput>
+							<Button title="Add tag" onPress={addTagHandler}></Button>
+						</View>
+					</View>
+				</View>
+			</View>
+		);
 
-      }
+		previousTags = (
+			<View style={styles.tagContainer}>
+				<Text style={styles.title}>Previously used tags</Text>
+				<View style={styles.tagList}>{userTagsArray}</View>
+			</View>
+		);
+	}
 
-    } catch (err) {
-      console.log("not here")
-
-      // setNewDocument({uri:'',name:''})
-      setNewDocument(null)
-
-    throw err;
-      
-    }
-  };
-
-  let documentDetails = (<Text>No file picked</Text>)
-  if (newDocument){
-    documentDetails =(<View>
-      <View style={styles.imgContainer}>
-        <Image style = {styles.image} source={{uri:newDocument.uri}}/>
-        </View>
-        <View style={styles.tagContainer}>
-          <Text style= {styles.title}>Add/remove tags</Text>
-          <View style= {styles.tagList}>{tagArray}</View>
-          <View style={styles.form}>
-            <View style={styles.inputGroup}>
-              <TextInput style={styles.input} keyboardType='default' onChangeText={inputChangedHandler} value={newTag}></TextInput>
-              <Button title="Add tag" onPress={addTagHandler}></Button>
-              </View>
-              </View>
-              </View>
-              </View>)}
-
-
-  return (<View style ={styles.main}> 
-            <Card style={styles.card}>
-            <View style={styles.btnContainer}>
-    <TouchableOpacity
-          style={styles.buttonStyle}
-          activeOpacity={0.5}
-          onPress={selectFile}>
-          <Text style={styles.buttonTextStyle}>Select File</Text>
-
-        </TouchableOpacity>
-         <TouchableOpacity
-          style={styles.buttonStyle}
-          activeOpacity={0.5}
-          onPress={()=>{createDocumentHandler(newDocument,tagList,userId,token)}
-          
-          }>
-          <Text style={styles.buttonTextStyle}>Upload File</Text>
-        </TouchableOpacity>
-        
-        </View>
-        {documentDetails}
-</Card>
-      </View>   
-
-
-  
-        
-
-  )}
-AddDocumentScreen.navigationOptions={
-  headerTitle:"Please Upload"
-}
+	return (
+		<View style={styles.main}>
+			<Card style={styles.card}>
+				<View style={styles.btnContainer}>
+					<TouchableOpacity
+						style={styles.buttonStyle}
+						activeOpacity={0.5}
+						onPress={selectFile}
+					>
+						<Text style={styles.buttonTextStyle}>Select File</Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.buttonStyle}
+						activeOpacity={0.5}
+						onPress={() => {
+							createDocumentHandler(newDocument, tagList, userId, token);
+						}}
+					>
+						<Text style={styles.buttonTextStyle}>Upload File</Text>
+					</TouchableOpacity>
+				</View>
+				{documentDetails}
+				{previousTags}
+			</Card>
+		</View>
+	);
+};
+AddDocumentScreen.navigationOptions = {
+	headerTitle: "Please Upload",
+};
 
 const styles = StyleSheet.create({
-  imgContainer:{
-    width:'100%',
-    height:'50%',
-    borderTopLeftRadius:10,
-    borderTopRightRadius:10,
-    overflow:'hidden'
-},
-main:{
-  flex:1,
-  // margin:10,
-  // justifyContent:'center'
-},
+	imgContainer: {
+		width: "100%",
+		height: "50%",
+		borderTopLeftRadius: 10,
+		borderTopRightRadius: 10,
+		overflow: "hidden",
+	},
+	main: {
+		flex: 1,
+		// margin:10,
+		// justifyContent:'center'
+	},
 
-    card:{
-flex:1,
-margin:5,
-// justifyContent:'center',
-    // alignItems:'center',
-    // height:'100%'
-  },
-  tagContainer:{
-    borderRadius:8,
-    borderWidth:2,
-    borderColor:'grey',
-    margin:5,
-    flex:1
-  },
-  inputGroup:{
-    flexDirection: 'row',
-    // justifyContent:'space-between',
-    alignItems:'center',
-},
-  tagList:{
-    flex:1,
-    flexDirection:'row',
-    justifyContent:'center',
-    alignItems:'center'
-},
-image:{
-  width:'100%',
-  height:'100%'
-},
-title:{
-  fontSize:20,
-  color:Colors.primary,
-  textAlign:'center',
-  marginVertical:20,
+	card: {
+		flex: 1,
+		margin: 5,
+		// justifyContent:'center',
+		// alignItems:'center',
+		// height:'100%'
+	},
+	tagContainer: {
+		borderRadius: 8,
+		borderWidth: 2,
+		borderColor: "grey",
+		margin: 5,
+		flex: 1,
+	},
+	inputGroup: {
+		flexDirection: "row",
+		// justifyContent:'space-between',
+		alignItems: "center",
+	},
+	tagList: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	image: {
+		width: "100%",
+		height: "100%",
+	},
+	title: {
+		fontSize: 20,
+		color: Colors.primary,
+		textAlign: "center",
+		marginVertical: 20,
+	},
+	tag: {
+		borderRadius: 4,
+		borderColor: "grey",
+		borderWidth: 2,
+		elevation: 3,
+		backgroundColor: "white",
+		margin: 5,
+	},
+	tagText: {
+		fontSize: 14,
+		padding: 5,
+	},
+	form: {
+		margin: 5,
+	},
+	label: {
+		marginVertical: 8,
+	},
+	input: {
+		padding: 5,
+		borderBottomColor: "grey",
+		borderBottomWidth: 1,
+		width: "70%",
+	},
 
-},
-tag:{
-  borderRadius:4,
-  borderColor:'grey',
-  borderWidth:2,
-  elevation:3, 
-  backgroundColor:'white',
-  margin:5
+	btnContainer: {
+		flexDirection: "row",
+		marginTop: 5,
+	},
 
-},
-tagText:{
-  fontSize:14,
-  padding:5
-},
-form:{
-  margin:5
-},
-label:{
-  marginVertical:8
-},
-input:{
-  padding:5,
-  borderBottomColor:"grey",
-  borderBottomWidth:1,
-  width:'70%'
-},
+	// imgcontainer:{
+	//   width:'100%',
+	//   height:200,
+	//   borderTopLeftRadius:10,
+	//   borderTopRightRadius:10,
+	//   // overflow:'hidden',
+	//   borderColor:'red',
+	//   borderWidth:1
+	// },
 
-
-btnContainer:{
-  flexDirection:'row',
-  marginTop:5
-
-},
-
-// imgcontainer:{
-//   width:'100%',
-//   height:200,
-//   borderTopLeftRadius:10,
-//   borderTopRightRadius:10,
-//   // overflow:'hidden',
-//   borderColor:'red',
-//   borderWidth:1
-// },
-
-buttonStyle:{
-  borderWidth:1,
-  borderColor:'lightgrey',
-  padding:5,
-  margin:10
-
-
-
-},
-buttonTextStyle:{
-  color:Colors.accent
-
-}
+	buttonStyle: {
+		borderWidth: 1,
+		borderColor: "lightgrey",
+		padding: 5,
+		margin: 10,
+	},
+	buttonTextStyle: {
+		color: Colors.accent,
+	},
 });
-export default AddDocumentScreen
+export default AddDocumentScreen;
